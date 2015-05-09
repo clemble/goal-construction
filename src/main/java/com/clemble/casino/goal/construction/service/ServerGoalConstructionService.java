178cda3b088960c8fca0a1c5775febbde8d70116
Anton O <mavarazy@gmail.com>
2015-05-09 19:02:33 +0300

    Removing Initiation Step

--- src/main/java/com/clemble/casino/goal/construction/service/ServerGoalConstructionService.java
+++ src/main/java/com/clemble/casino/goal/construction/service/ServerGoalConstructionService.java
@@ -1,25 +1,50 @@
 package com.clemble.casino.goal.construction.service;
 
+import com.clemble.casino.error.ClembleCasinoError;
+import com.clemble.casino.error.ClembleCasinoException;
+import com.clemble.casino.goal.GoalAware;
+import com.clemble.casino.goal.construction.GoalKeyGenerator;
+import com.clemble.casino.goal.lifecycle.configuration.GoalConfiguration;
 import com.clemble.casino.goal.lifecycle.construction.service.GoalConstructionService;
 import com.clemble.casino.lifecycle.construction.ConstructionState;
 import com.clemble.casino.goal.lifecycle.construction.GoalConstruction;
 import com.clemble.casino.goal.lifecycle.construction.GoalConstructionRequest;
 import com.clemble.casino.goal.construction.repository.GoalConstructionRepository;
+import com.clemble.casino.money.Money;
+import com.clemble.casino.payment.service.PlayerAccountService;
 import com.clemble.casino.server.ServerService;
+import com.clemble.casino.server.event.SystemEvent;
+import com.clemble.casino.server.event.goal.SystemGoalStartedEvent;
+import com.clemble.casino.server.event.payment.SystemPaymentFreezeRequestEvent;
+import com.clemble.casino.server.player.notification.SystemNotificationService;
+import org.jboss.logging.MDC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Created by mavarazy on 9/10/14.
  */
 public class ServerGoalConstructionService implements GoalConstructionService, ServerService {
 
-    final private GoalConstructionRepository constructionRepository;
-    final private SelfGoalConstructionService selfGoalConstructionService;
+    final private Logger LOG = LoggerFactory.getLogger(GoalConstructionService.class);
 
-    public ServerGoalConstructionService(SelfGoalConstructionService selfGoalConstructionService, GoalConstructionRepository constructionRepository) {
+    final private GoalKeyGenerator keyGenerator;
+    final private SystemNotificationService notificationService;
+    final private PlayerAccountService accountService;
+    final private GoalConstructionRepository constructionRepository;
+
+    public ServerGoalConstructionService(
+        GoalKeyGenerator keyGenerator,
+        PlayerAccountService accountService,
+        SystemNotificationService notificationService,
+        GoalConstructionRepository constructionRepository) {
+        this.keyGenerator = keyGenerator;
+        this.notificationService = notificationService;
         this.constructionRepository = constructionRepository;
-        this.selfGoalConstructionService = selfGoalConstructionService;
+        this.accountService = accountService;
     }
 
     @Override
@@ -28,7 +53,33 @@ public class ServerGoalConstructionService implements GoalConstructionService, S
     }
 
     public GoalConstruction construct(String player, GoalConstructionRequest request) {
-        return selfGoalConstructionService.construct(player, request);
+        if (!(request.getConfiguration() instanceof GoalConfiguration))
+            throw new IllegalArgumentException();//TODO add specific error for that
+        // Step 1. Checking this is appropriate request for this service
+        if (request.getGoal() == null || request.getGoal().isEmpty())
+            throw ClembleCasinoException.fromError(ClembleCasinoError.GoalIsEmpty, player);
+        // Step 1.1. Checking there is enough money to complete it
+        Money price = request.getConfiguration().getBet().getAmount();
+        if (!accountService.canAfford(Collections.singleton(player), price.getCurrency(), price.getAmount()).isEmpty()){
+            throw ClembleCasinoException.fromError(ClembleCasinoError.GameConstructionInsufficientMoney, player);
+        }
+        String goalKey = keyGenerator.generate(player);
+        // Step 2. Creating new GoalConstruction
+        MDC.put(GoalAware.GOAL_KEY, goalKey);
+        LOG.debug("1. Creating new");
+        GoalConstruction construction = request.toConstruction(player, goalKey);
+        construction = construction.clone(ConstructionState.constructed);
+        // Step 3. Saving game construction
+        LOG.debug("2. Saving new");
+        GoalConstruction savedConstruction =  constructionRepository.save(construction);
+        // Step 4. Returning saved construction
+        LOG.debug("3. Freezing amount for a player");
+        SystemEvent freezeRequest = SystemPaymentFreezeRequestEvent.create(goalKey, player, price);
+        notificationService.send(freezeRequest);
+        LOG.debug("4. Notifying of goal started event, for manager processing");
+        notificationService.send(new SystemGoalStartedEvent(goalKey, savedConstruction));
+        LOG.debug("5. done");
+        return savedConstruction;
     }
 
     @Override
--- src/main/java/com/clemble/casino/goal/construction/spring/GoalConstructionSpringConfiguration.java
+++ src/main/java/com/clemble/casino/goal/construction/spring/GoalConstructionSpringConfiguration.java
@@ -2,19 +2,11 @@ package com.clemble.casino.goal.construction.spring;
 
 import com.clemble.casino.goal.construction.GoalKeyGenerator;
 import com.clemble.casino.goal.construction.controller.GoalConstructionController;
-import com.clemble.casino.goal.construction.controller.GoalInitiationController;
-import com.clemble.casino.goal.construction.listener.SystemGoalInitiationDueEventListener;
-import com.clemble.casino.goal.construction.listener.SystemGoalInitiationStartedEventListener;
 import com.clemble.casino.goal.construction.repository.GoalConstructionRepository;
-import com.clemble.casino.goal.construction.repository.GoalInitiationRepository;
-import com.clemble.casino.goal.construction.service.SelfGoalConstructionService;
 import com.clemble.casino.goal.construction.service.ServerGoalConstructionService;
-import com.clemble.casino.goal.construction.service.ServerGoalInitiationService;
 import com.clemble.casino.payment.service.PlayerAccountService;
 import com.clemble.casino.server.key.RedisKeyFactory;
-import com.clemble.casino.server.player.notification.ServerNotificationService;
 import com.clemble.casino.server.player.notification.SystemNotificationService;
-import com.clemble.casino.server.player.notification.SystemNotificationServiceListener;
 import com.clemble.casino.server.spring.common.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -40,13 +32,16 @@ public class GoalConstructionSpringConfiguration {
     }
 
     @Bean
-    public GoalInitiationRepository goalInitiationRepository(MongoRepositoryFactory repositoryFactory) {
-        return repositoryFactory.getRepository(GoalInitiationRepository.class);
-    }
-
-    @Bean
-    public ServerGoalConstructionService goalConstructionService(SelfGoalConstructionService constructionService, GoalConstructionRepository repository) {
-        return new ServerGoalConstructionService(constructionService, repository);
+    public ServerGoalConstructionService goalConstructionService(
+        GoalKeyGenerator keyGenerator,
+        @Qualifier("playerAccountClient") PlayerAccountService accountService,
+        SystemNotificationService notificationService,
+        GoalConstructionRepository constructionRepository) {
+        return new ServerGoalConstructionService(
+            keyGenerator,
+            accountService,
+            notificationService,
+            constructionRepository);
     }
 
     @Bean
@@ -55,53 +50,8 @@ public class GoalConstructionSpringConfiguration {
     }
 
     @Bean
-    public GoalInitiationController goalInitiationServiceController(ServerGoalInitiationService initiationService) {
-        return new GoalInitiationController(initiationService);
-    }
-
-    @Bean
     public GoalKeyGenerator goalKeyGenerator(JedisPool jedisPool) {
         return new GoalKeyGenerator(new RedisKeyFactory("GOAL_COUNTER", "A", jedisPool));
-    }
-
-    @Bean
-    public ServerGoalInitiationService serverGoalInitiationService(
-        GoalInitiationRepository initiationRepository,
-        @Qualifier("playerNotificationService") ServerNotificationService notificationService,
-        SystemNotificationService systemNotificationService){
-        return new ServerGoalInitiationService(initiationRepository, notificationService, systemNotificationService);
-    }
-
-    @Bean
-    public SelfGoalConstructionService selfGoalConstructionService(
-        GoalKeyGenerator keyGenerator,
-        GoalConstructionRepository constructionRepository,
-        SystemNotificationService notificationService,
-        @Qualifier("playerAccountClient") PlayerAccountService accountServiceContract) {
-        return new SelfGoalConstructionService(keyGenerator, notificationService, constructionRepository, accountServiceContract);
-    }
-
-    @Bean
-    public SystemGoalInitiationDueEventListener systemGoalInitiationExpirationEventListener(
-        SystemNotificationServiceListener notificationServiceListener,
-        SystemNotificationService notificationService,
-        GoalInitiationRepository initiationRepository) {
-        SystemGoalInitiationDueEventListener eventListener = new SystemGoalInitiationDueEventListener(notificationService, initiationRepository);
-        notificationServiceListener.subscribe(eventListener);
-        return eventListener;
-    }
-
-    @Bean
-    public SystemGoalInitiationStartedEventListener systemGoalInitiationStartedEventListener(
-        SystemNotificationServiceListener notificationServiceListener,
-        ServerGoalInitiationService initiationService
-    ) {
-        // Step 1. Generating event listener
-        SystemGoalInitiationStartedEventListener eventListener = new SystemGoalInitiationStartedEventListener(initiationService);
-        // Step 2. Subscribing to event listener
-        notificationServiceListener.subscribe(eventListener);
-        // Step 3. Returning event listener
-        return eventListener;
     }
 
 }
